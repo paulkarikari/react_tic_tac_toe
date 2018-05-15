37e8f3a7de8718fda1a3849663446b26f75515b6
paul karikari <paulkarikari1@gmail.com>
2018-05-15 04:18:59 +0000

    pass handle child as props to square

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: null
-		};
-	}
-
 	render() {
 		return (
-			<button className="square" onClick={() => this.setState({ value: 'X' })}>
-				{this.state.value}
+			<button className="square" onClick={() => this.props.onClick()}>
+				{this.props.value}
 			</button>
 		);
 	}
@@ -27,8 +20,14 @@ class Board extends React.Component {
 		};
 	}
 
+	handleChild(i) {
+		const squares = this.state.squares.slice();
+		squares[i] = 'X';
+		this.setState({ squares });
+	}
+
 	renderSquare(i) {
-		return <Square value={this.state.squares[i]} />;
+		return <Square value={this.state.squares[i]} onClick={() => this.handleChild(i)} />;
 	}
 
 	render() {
